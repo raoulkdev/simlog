582f6eac9492770b869edc671515ccfb37fab0fa
Raoul Kaleba <raouldev@outlook.com>
2026-07-08 15:14:56 -0500

    Setup home page and changed header

--- frontend/simlog_frontend/src/components/Header.jsx
+++ frontend/simlog_frontend/src/components/Header.jsx
@@ -5,16 +5,18 @@ function Header() {
   const navigate = useNavigate();
   
   return (
-    <div className='flex gap-4 p-4 bg-black'>
-      <button onClick={() => navigate('/')} className="bg-white text-black px-2 py-2 rounded">
-        Home
+    <div className='flex justify-between p-4 bg-blue-950'>
+      <button onClick={() => navigate('/')} className=" text-white text-2xl font-bold px-2 py-2 rounded">
+        Simlog
       </button>
-      <button onClick={() => navigate('/add_flight')} className="bg-white text-black px-2 py-2 rounded">
-        Post a new flight
-      </button>
-      <button onClick={() => navigate('/flights')} className="bg-white text-black px-2 py-2 rounded">
-        View All Flights
-      </button>
+      <div className='flex gap-4'>
+        <button onClick={() => navigate('/add_flight')} className="bg-white text-black px-2 py-2 rounded">
+          Post a new flight
+        </button>
+        <button onClick={() => navigate('/flights')} className="bg-white text-black px-2 py-2 rounded">
+          View All Flights
+        </button>
+      </div>
     </div>
   );
 }
--- frontend/simlog_frontend/src/pages/Home.jsx
+++ frontend/simlog_frontend/src/pages/Home.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className='bg-black'>
-      <h2 className='text-white'>Simlog home page</h2>
+    <div className='flex flex-col items-center justify-center text-center gap-4 min-h-screen bg-black px-4'>
+      <h1 className='text-white text-6xl font-bold'>Welcome to Simlog</h1>
+      <p className='text-white text-lg max-w-300'>
+        Simlog is a flight logging app for flight simulator pilots.
+        It lets users upload and manage detailed flight records
+        (aircraft, route, duration, weather, approach/landing quality, notes, etc.),
+        browse individual flights or their full history,
+        and ask an AI assistant questions about a specific flight for quick insights and recall.
+      </p>
     </div>
   );
 }
